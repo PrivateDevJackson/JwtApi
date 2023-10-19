410c7fa01e51bc0214a3cec9cd26dd258d324b99
Private Jackson <141220163+PrivateDevJackson@users.noreply.github.com>
2023-10-19 14:03:35 +0200

    Update Program.cs

--- JwtApi/Program.cs
+++ JwtApi/Program.cs
@@ -4,14 +4,13 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using Microsoft.IdentityModel.Tokens;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
